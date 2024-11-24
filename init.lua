efcde4194b79872fce536459d3c2b6b29b5ca67c
griffinaddison <gaddison@seas.upenn.edu>
2024-11-24 00:12:08 -0500

    added nvim-surround. added obsidian.nvim

--- init.lua
+++ init.lua
@@ -59,6 +59,9 @@ vim.keymap.set("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewis
 -- Toggle comment for a visual selection
 vim.keymap.set("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { noremap = true, silent = true })
 
+-- How much to 'conceal' formatting characters in, for example, .md files. 0, 1, or 2
+vim.opt.conceallevel = 1
+
 
 -- Copilot: <leader><Tab> normal mode to toggle, <Tab> insert mode to accept
 local function toggle_copilot_auto_trigger()
@@ -299,6 +302,17 @@ require("lazy").setup({
 	},
 
 	{
+			"kylechui/nvim-surround",
+			version = "*", -- Use for stability; omit to use `main` branch for the latest features
+			event = "VeryLazy",
+			config = function()
+					require("nvim-surround").setup({
+							-- Configuration here, or leave empty to use defaults
+					})
+			end
+	},
+
+	{
 		"stevearc/oil.nvim",
 		config = function()
 			require("oil").setup({
@@ -315,6 +329,42 @@ require("lazy").setup({
 			})
 		end,
 	},
+
+
+	-- {
+	--   "epwalsh/obsidian.nvim",
+	--   version = "*",  -- recommended, use latest release instead of latest commit
+	--   lazy = true,
+	--   ft = "markdown",
+	--   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	--   -- event = {
+	--   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	--   --   -- refer to `:h file-pattern` for more examples
+	--   --   "BufReadPre path/to/my-vault/*.md",
+	--   --   "BufNewFile path/to/my-vault/*.md",
+	--   -- },
+	--   dependencies = {
+	-- 	-- Required.
+	-- 	"nvim-lua/plenary.nvim",
+	--
+	-- 	-- see below for full list of optional dependencies 👇
+	--   },
+	--   opts = {
+	-- 	workspaces = {
+	-- 	  {
+	-- 		name = "personal",
+	-- 		path = "~/vaults/personal",
+	-- 	  },
+	-- 	 --  {
+	-- 		-- name = "work",
+	-- 		-- path = "~/vaults/work",
+	-- 	 --  },
+	-- 	},
+	--
+	-- 	-- see below for full list of options 👇
+	--   },
+	-- },
 
 
 	-- theme
